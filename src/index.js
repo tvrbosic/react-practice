6df4abd419006dbcea17f56fcc7a3ba8fdd1b23e
tvrbosic <tomislav.vrbosic@gmail.com>
2022-06-11 08:30:27 +0200

    11_06_2022

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Import the React and ReactDom Libraries
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import SeasonDisplay from "./components/SeasonDisplay";
+import Loader from "./components/Loader";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+// Create React Component
+class App extends React.Component {
+  state = { lat: null, errorMessage: ''};
+
+  componentDidMount() {
+    console.log('My Component was rendered on screen!');
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  };
+
+  componentDidUpdate() {
+    console.log('My Component was just updated!');
+  };
+
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    else if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat}/>;
+    }
+    else {
+      return <Loader message={"Loading location..."}/>
+    };
+  };
+
+  render() {
+    return <div className="border red">
+      {this.renderContent()}
+    </div>  
+  };
+}
+
+// Take the React component and show it on the screen
+root.render(<App />);
