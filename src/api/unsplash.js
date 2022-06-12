aebbd91c72a052c5cf4e104280214709e79eecc8
tvrbosic <tomislav.vrbosic@gmail.com>
2022-06-12 12:32:32 +0200

    12_06_2022

---
+++ src/api/unsplash.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'https://api.unsplash.com',
+    headers: {
+        Authorization: 'Client-ID p9G7Ibfugm2v6mORms8j8dYgdTqASYYgnv9hvbwq2rM'
+    }
+});
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,15 @@ import React from 'react';
 class SearchBar extends React.Component {
     state = { text: 'Hi there!' };
 
+    onFormSubmit(event) {
+        event.preventDefault();
+        this.props.onSubmit(this.state.text);
+    }
+
     render () {
         return (
             <div className='ui segment'>
-                <form className='ui form'>
+                <form className='ui form' onSubmit={ (e) => this.onFormSubmit(e) }>
                     <div className='field'>
                         <label>Image search</label>
                         <input 
