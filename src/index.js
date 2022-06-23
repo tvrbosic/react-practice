5740fdc54eef7ac7c9b95dfa8f8d085b150e5b62
tvrbosic <tomislav.vrbosic@gmail.com>
2022-06-23 15:38:14 +0200

    23_06_2022

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 // Import the React and ReactDom Libraries
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// Import application
+// Redux
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+// Import application and components
 import App from './components/App';
+import reducers from './reducers';
 
 // Get root element and create React root element
 const container = document.getElementById('root');
 const root = createRoot(container);
 
+// For Redux we have to wrap App with Provider
 // Render Application inside React root element
-root.render(<App />);
+root.render(
+	<Provider store={createStore(reducers)}>
+		<App />
+	</Provider>
+);
