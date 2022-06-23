bec4e63f9553484a266122407f442030266f8be6
tvrbosic <tomislav.vrbosic@gmail.com>
2022-06-23 08:08:43 +0200

    23_06_2022

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import SearchBar from './SearchBar';
-import VideoList from './VideoList';
-import VideoDetails from './VideoDetail';
-import useVideos from '../hooks/useVideos';
+import React from 'react';
 
 const App = () => {
-    const [selectedVideo, setSelectedVideo] = useState(null);
-    
-    // Custom hook, returns list of videos and search video function
-    const [videos, search] = useVideos('StarCraft');
-    
-    useEffect(() => {
-        setSelectedVideo(videos[0])
-    }, [videos]);
-
-    return (
-        <div className='ui container'>
-            <SearchBar onSearchSubmit={search} />
-            <div className='ui grid'>
-                <div className='ui row'>
-                    <div className='eleven wide column'>
-                        <VideoDetails video={selectedVideo} />
-                    </div>
-                    <div className='five wide column'>
-                        <VideoList videos={videos} onVideoSelect={setSelectedVideo} />
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+    return <div>App</div>;
 };
 
 export default App;
