59feac8af36e3941fbb56f995ab563cf01ed75a8
tvrbosic <tomislav.vrbosic@gmail.com>
2022-06-29 11:45:16 +0200

    29_06_2022

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,18 @@
-import _ from 'lodash';
 import jsonPlaceholder from '../api/jsonPlaceholder';
+import _ from 'lodash';
+
+
+// Action Creator
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+	// Dispatch Action creator that will get invoked, wait for it to finish (update state with results) 
+	await dispatch(fetchPosts());
+
+	// Map userIds to array and make it unique
+	const userIds = _.uniq(_.map(getState().posts, 'userId'));
+
+	// For each user id, fetch user data
+	userIds.forEach(id => dispatch(fetchUser(id)));
+};
 
 // Action Creator
 export const fetchPosts = () => {
@@ -10,9 +23,22 @@ export const fetchPosts = () => {
 	};
 };
 
+// Action Creator
+export const fetchUser = id => async dispatch => {
+	const response = await jsonPlaceholder.get(`/users/${id}`);
+
+	dispatch({ type: `FETCH_USER`, payload: response.data });
+};
+
+/**
+#### Fetch user with memoize utility function #### 
+
+import _ from 'lodash';
+
 export const fetchUser = id => dispatch => {
 	_fetchUser(id, dispatch);
 };
+
 
 const _fetchUser = _.memoize((id, dispatch) => {
 	return async (dispatch, getState) => {
@@ -21,3 +47,4 @@ const _fetchUser = _.memoize((id, dispatch) => {
 		dispatch({ type: `FETCH_USER`, payload: response.data });
 	};
 });
+ */
