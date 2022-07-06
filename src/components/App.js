eb81d6745015e06aa82b03e7968f949b5f475399
tvrbosic <tomislav.vrbosic@gmail.com>
2022-07-06 15:36:31 +0200

    06_07_2022

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import PostList from './PostList';
+import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
-const App = () => {
-	return (
-		<div className='ui container'>
-			<PostList />
-		</div>
-	);
-};
+class App extends React.Component {
+	state = {
+		language: 'english',
+	};
+
+	onLanguageChange = (language) => {
+		this.setState({ language: language });
+	};
+
+	render() {
+		return (
+			<div className='ui container'>
+				<div>
+					Select a language:
+					<i className='flag us' onClick={() => this.onLanguageChange('english')} />
+					<i className='flag hr' onClick={() => this.onLanguageChange('croatian')} />
+				</div>
+				<ColorContext.Provider value={'primary'}>
+					<LanguageContext.Provider value={this.state.language}>
+						<UserCreate />
+					</LanguageContext.Provider>
+				</ColorContext.Provider>
+			</div>
+		);
+	}
+}
 
 export default App;
---
+++ src/contexts/LanguageContext.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+// Default value: 'english'
+export default React.createContext('english');
