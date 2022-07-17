9785c4c8309a2a7770aeacc048d50c2f3835ed50
tvrbosic <tomislav.vrbosic@gmail.com>
2022-07-17 19:21:08 +0200

    17_07_2022

---
+++ 01-expense-tracker/src/App.js
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NexExpense';
+
+const exampleExpenses = [
+   {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+   },
+   { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+   {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+   },
+   {
+      id: 'e4',
+      title: 'New Wooden Desk',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+   },
+];
+
+const App = () => {
+   const [expenses, setExpenses] = useState(exampleExpenses);
+
+   const addNewExpense = (expenseData) => {
+      setExpenses((prevState) => {
+         return [ ...prevState, expenseData ];
+      });
+   };
+
+   return (
+      <div>
+         <NewExpense onSaveExpenseData={addNewExpense} />
+         <Expenses items={expenses} />
+      </div>
+   );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 
+import './index.css';
 import App from './App';
 
-const root = createRoot(document.getElementById('root'));
-
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
